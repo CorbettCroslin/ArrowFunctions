09bdbe777c8d47ee33240448a2bbe45f1d705e42
Corbett <corbett.croslin@gmail.com>
2021-10-17 19:17:04 -0400

    NEW

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+function getSum (a,b) {
+    console.log(a + b);
+
+}
+getSum(8,4); 
+
+let sum = (a,b) => a + b;
+alert ( sum(8,4));
+
+
+
+function getSub (a,b) {
+    console.log(a - b);
+    
+}
+getSub(8,4);
+
+let sub = (a,b) => a - b;
+alert ( sub(8,4));
+
+
+
+function getMul (a,b) {
+    console.log(a * b);
+    
+}
+getMul(8,4);
+
+let mul = (a,b) => a * b;
+alert ( mul(8,4));
+
+
+
+function getDiv (a,b) {
+    console.log(a / b);
+
+}
+getDiv(8,4);
+
+let div = (a,b) => a / b;
+alert ( div(8,4));
+
